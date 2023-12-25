b52ab146cfe3d1c895fed5e2594913e0b073819e
MFakaz <121366828+MFakaz@users.noreply.github.com>
2023-12-25 07:22:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,65 @@
-// Contoh data tugas
-var tugas1 = { nama: "Matematika", deadline: "2023-01-30", status: "Belum selesai" };
-var tugas2 = { nama: "Bahasa Indonesia", deadline: "2023-02-15", status: "Belum selesai" };
+body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  padding: 0;
+}
 
-// Daftar tugas
-var daftarTugas = [tugas1, tugas2];
+nav {
+  background-color: #333;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+}
 
-// Tambahkan tugas ke daftar secara dinamis
-var daftarTugasElement = document.getElementById("daftar-tugas");
-daftarTugas.forEach(function(tugas) {
-    var li = document.createElement("li");
-    li.innerHTML = `
-        <strong>Nama Tugas:</strong> ${tugas.nama}
-        <br>
-        <strong>Deadline:</strong> ${tugas.deadline}
-        <br>
-        <strong>Status:</strong> ${tugas.status}
-    `;
-    daftarTugasElement.appendChild(li);
-});
+.menu-icon {
+  font-size: 1.5em;
+  cursor: pointer;
+  display: none;
+}
+
+.nav-list {
+  list-style: none;
+  display: flex;
+}
+
+.nav-list li {
+  margin-right: 20px;
+}
+
+.nav-list a {
+  text-decoration: none;
+  color: white;
+}
+
+.content {
+  padding: 20px;
+}
+
+@media only screen and (max-width: 768px) {
+  .nav-list {
+    display: none;
+    flex-direction: column;
+    position: absolute;
+    top: 60px;
+    left: 0;
+    width: 100%;
+    background-color: #333;
+  }
+
+  .nav-list.show {
+    display: flex;
+  }
+
+  .nav-list li {
+    margin: 0;
+    text-align: center;
+    padding: 10px;
+    border-bottom: 1px solid white;
+  }
+
+  .menu-icon {
+    display: block;
+  }
+}
